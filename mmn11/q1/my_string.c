43d2aeb0580690e625bccae11def8df2b818a987
Almog Shtaigmann <a.shtaigmann@gmail.com>
2023-03-08 22:24:58 +0200

    q1 done

--- mmn11/q1/my_string.c
+++ mmn11/q1/my_string.c
@@ -9,12 +9,30 @@ strchr -> my_strchr
 #include <stdio.h>
 #include <string.h>
 
-int my_strncmp(const char s1[], const char s2[], size_t n)
+#define MAX_SIZE 80
+
+int my_strchr(const char str[], char c)
 {
     int i = 0;
+    /* search the char c until you reach to the end of the string*/
+    while (str[i] != '\0')
+    {
+        /*When found the char, return the index*/
+        if (str[i] == c)
+            return i;
+        /*Keep search for c in str*/
+        i++;
+    }
+    /*THe char was not found -> return -1*/
+    return -1;
+}
+
+int my_strncmp(char s1[], char s2[], int n)
+{
+    int i;
     for (i = 0; i < n; i++)
     {
-        if (s1[i] != s2[2])
+        if (s1[i] != s2[i])
         {
             /* return difference between first differing characters */
             return s1[i] - s2[i];
@@ -25,6 +43,7 @@ int my_strncmp(const char s1[], const char s2[], size_t n)
             return 0;
         }
     }
+    return 0;
 }
 
 int my_strcmp(const char s1[], const char s2[])
@@ -44,29 +63,56 @@ int my_strcmp(const char s1[], const char s2[])
 
 int main()
 {
-    /*
-    char s1[] = "kasl", s2[] = "kab";
-    int result, result2;
-    result = strcmp(s1, s2);
-    printf("The original function returned: %d\n", result);
+    char str1[MAX_SIZE], str2[MAX_SIZE];
+    int k, result;
+    char c;
+    printf("Hi there!\n-------\n");
 
-    result2 = my_strcmp(s1, s2);
-    printf("MY function returned: %d", result2);
+    printf("Function 1, my_strncmp\n");
+    printf("Enter the first string\n");
+    scanf("%s", str1);
+    printf("Ok, str1 is: %s\n", str1);
 
-     _________________________*/
+    printf("Enter the second string\n");
+    scanf("%s", str2);
+    printf("Ok, str2 is: %s\n", str2);
 
-    char s1[] = "1234", s2[] = "123456", s3[10];
-    int n;
-    n = 3;
-    int result, result2, res3;
-    result = strncmp(s1, s2, n);
-    printf("The original function returned: %d\n", result);
+    result = my_strcmp(str1, str2);
 
-    result2 = my_strncmp(s1, s2, n);
-    printf("MY function returned: %d\n", result2);
+    printf("The result for my_strcmp(st1,str2) is: %d \n", result);
+    printf("The result for strcmp(st1,str2) is: %d \n\n", strcmp(str1, str2));
 
-    res3 = tets1(s1, s2, 12);
-    printf("MY function returned: %d\n", res3);
+    /*set str1,str2 as 0*/
+    memset(str1, 0, sizeof(str1));
+    memset(str2, 0, sizeof(str2));
+
+    printf("\nFunction 2, my_strncmp\n");
+    printf("Enter the first string\n");
+    scanf("%s", str1);
+    printf("Ok, str1 is: %s\n", str1);
+
+    printf("Enter the second string\n");
+    scanf("%s", str2);
+    printf("Ok, str2 is: %s\n", str2);
+
+    printf("enter the index\n");
+    scanf("%d", &k);
+    printf("Ok, k is: %d\n", k);
+
+    printf("The result for my_strncmp(st1,str2,k) is: %d \n", my_strncmp(str1, str2, k));
+    printf("The result for strncmp(st1,str2,k) is: %d \n\n", strncmp(str1, str2, k));
+
+    /*set str1,str2 as 0*/
+    memset(str1, 0, sizeof(str1));
+
+    printf("\n\nFunction 3, my_strchr\n");
+    printf("Enter a string and then char\n");
+    scanf("%s %c", str1, &c);
+    printf("Ok,str is: %s and c is: %c\n", str1, c);
+
+    result = my_strchr(str1, c);
+
+    printf("The result for my_strchr(st1,c) is: %d \n", result);
 
     return 0;
 }
