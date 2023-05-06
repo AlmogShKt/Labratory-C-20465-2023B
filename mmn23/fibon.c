7a812067fb43a395bfa91edadde615a4cd8486b2
Almog Shtaigmann <a.shtaigmann@gmail.com>
2023-05-06 15:20:00 +0300

    added coments, fix bugs, and free memory

--- mmn23/fibon.c
+++ mmn23/fibon.c
@@ -1,55 +1,116 @@
-//
-// Created by Almog Shtaigmann on 04/05/2023.
-//
+/**
+* Created by Almog Shtaigmann on 04/05/2023.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <errno.h>
 #include "fibonacci_handler.h"
 
 #define MAX_SIZE 1000
+#define MAX_INT 20
 
 int isFileExist(char path[]);
 
-
+/**
+@brief Main function of the mmn23 program.
+This function is the main entry point of the mmn23 program. The program expects one command line argument, which is
+the name of the file to output the Fibonacci sequence to. The program prompts the user to enter an integer 'n', which
+is the number of Fibonacci numbers to generate. The program then generates the first n Fibonacci numbers and outputs
+them to both the provided file and the terminal. If any errors occur during the program execution, the program will
+output an error message to the terminal.
+@param argc The number of command line arguments passed to the program.
+@param argv An array of strings representing the command line arguments passed to the program.
+*/
 int main(int argc, char *argv[]) {
-    Node *list_head = NULL;
-    FILE *file_ptr = NULL;
-    char file_name[MAX_SIZE];
-    int n;
-    int error_found = 0;
-    char welcome_file_msg[MAX_SIZE];
-
-
+    Node *list_head = NULL; /* A pointer to the head node of the linked list containing the Fibonacci numbers. */
+    FILE *file_ptr = NULL; /*A character array to store the name of the file to output the Fibonacci numbers to. */
+    char file_name[MAX_SIZE]; /*A character array to store the name of the file to output the Fibonacci numbers to. */
+    int n = 0; /*An integer representing the number of Fibonacci numbers to generate. */
+    int error_found = 0; /*A flag indicating whether an error was found during the program execution. */
+    char welcome_file_msg[MAX_SIZE]; /* A character array to store the welcome message to be printed to the file. */
+    char input[MAX_SIZE];
+    /*Check if the correct number of command line arguments was passed to the program. */
     if ((argc - 1) == 1) {
+        /*Get the file name from the command line argument. */
         strcpy(file_name, argv[1]);
-        if (isFileExist(file_name)) {
-            printf("Hi there, amd welcome to mmn23 !\n");
-            printf("Please enter a number. This number will set the length of the fibonacci sequence size\n");
-            scanf("%d", &n);
-            printf("Creating fibonacci sequence in size of %d", n);
 
+        /* Check if the file exists. */
+        if (isFileExist(file_name)) {
+            printf("Hi there, and welcome to mmn23 !\n");
+            printf("Please enter a number. This number will set the length of the fibonacci sequence size\n");
+
+            printf("Enter the number of Fibonacci numbers to generate: ");
+
+            /*Get the input from the user*/
+            fgets(input, MAX_INT, stdin);
+            /*Check if integer was provided on the input*/
+            if (sscanf(input, "%d", &n) != 1) {
+                /*There is no Integer in the input*/
+                error_found = 1;
+            } else {
+                /*For case the input was: 12as - there is integer but also invalid chars 'as'*/
+                for (int i = 0; i < strlen(input); i++) {
+                    /*Ignore '-' , EOF, new line */
+                    if (!isdigit(input[i]) && input[i] != '\n' && input[i] != EOF && input[i] != '-') {
+                        /*The input contain some invalid chars*/
+                        error_found = 1;
+                        break;
+                    }
+                }
+            }
+            /*Print msg if needed*/
+            if (error_found) {
+                printf("~~ERROR - Invalid input - not a number\n");
+                exit(1);
+            } else if (n < 0) {
+                printf("Invalid input - number less then 0\n");
+                exit(1);
+            }
+
+            printf("Creating fibonacci sequence in size of %d\n", n);
+
+            /* Generate the linked list containing the first n Fibonacci numbers. */
             error_found = create_fibonacci(&list_head, n);
+
+            /* If the linked list was successfully created, output the Fibonacci sequence to the file and the terminal. */
             if (!error_found) {
+                /* Open the file for writing. */
                 file_ptr = fopen(file_name, "w");
+
+                /* Construct the welcome message to be printed to the file. */
                 strcpy(welcome_file_msg, "Hi again, This is mmn23. \
             \nThis program will get a file name ot path, and a number 'n' \
             \nThen the program will calculate the first n nu,ber in fibonacci sequence. \
             \nThe program will print the sequence from the biggest nu,ber to the lower in the terminal as well in the provided file.");
+
+                /* Print the welcome message to the file. */
                 fprintf(file_ptr, "%s", welcome_file_msg);
+
+                /* Print the Fibonacci sequence to the file and the terminal. */
                 print_list(&list_head, file_ptr, n);
+
             } else {
-                printf("~~ERROR- Memory allocation failed");
+
+                /* Output an error message if the linked list creation failed. */
+                printf("~~ERROR- Memory allocation failed\n");
             }
+        } else {
+            if (argc == 2)
+                printf("~~ERROR- Could not open provided file name: %s \n", file_name);
+
+
         }
     } else {
+        /** Output an error message if an incorrect number of command line*/
         if (argc == 1)
             printf("~~ERROR- file name is missing");
-        if (argc > 2)
-            printf("~~ERROR- Could not open provided file name: %s ", file_name);
+        else
+            printf("~~ERROR- Too many arguments was provided in the command");
     }
-
+    free_list(list_head);
     return 0;
 }
 
@@ -57,7 +118,7 @@ int main(int argc, char *argv[]) {
 int isFileExist(char path[]) {
     FILE *file_ptr = fopen(path, "r");
     if (file_ptr == NULL) {
-
+        /*File does not exist*/
         return 0;
     }
     fclose(file_ptr);
--- mmn23/fibonacci_handler.c
+++ mmn23/fibonacci_handler.c
@@ -1,16 +1,148 @@
-//
-// Created by Almog Shtaigmann on 04/05/2023.
-//
+/**
+* Created by Almog Shtaigmann on 04/05/2023.
+*/
+#include "fibonacci_handler.h"
+#include <string.h>
 #include <stdio.h>
-
-typedef struct node {
-    unsigned long fib_value;      /* A pointer to a string variable to store the Fibonacci number. */
-    struct node *next;  /* A pointer to the next node in the list. */
-    struct node *prev;  /* A pointer to the previous node in the list. */
-} Node;
+#include <stdlib.h>
 
 
-int create_fibonacci(Node **head, int n);
-void print_list(Node **head, FILE *file_ptr, int n);
+/**
+ * Creates a new node for a linked list with the given data.
+ *
+ * @param value       The data to be stored in the new node.
+ * @return new_node - A pointer to the newly created node.
+ */
+Node *create_node(unsigned long int value) {
+    /* Allocate memory for the new node */
+    Node *new_node = (Node *) malloc(sizeof(Node));
 
+    /*Only if the allocation seceded*/
+    if (new_node != NULL) {
+        /*Set the value of the new node*/
+        new_node->fib_value = value;
+
+        /* Set the next pointer to NULL */
+        new_node->next = NULL;
+
+        /* Return a pointer to the newly created node */
+        return new_node;
+    } else
+        return NULL;
+}
+
+/**
+ * Inserts a new node containing the given string data into the circular linked list.
+ *
+ * @param head      A pointer to a pointer to the head node of the linked list.
+ * @param value      The string data to be stored in the new node.
+ */
+void insert_node(Node **head, unsigned long int value) {
+    Node *new_node = create_node(value);  /* create a new node */
+    if (new_node != NULL) {
+        if (*head == NULL) {  /* if the list is empty, set the new node as the head node */
+            *head = new_node;
+            new_node->next = *head;
+
+        } else {  /* otherwise, traverse the list to find the last node, and append the new node to it */
+            Node *temp = *head;
+            while (temp->next != *head) {
+                temp = temp->next;
+            }
+            /*Closing the 'circle'*/
+            temp->next = new_node;
+            new_node->prev = temp;
+            new_node->next = *head;
+            (*head)->prev = new_node;
+        }
+    } else {
+        return;
+    }
+}
+
+/**
+Generates a linked list of Fibonacci numbers up to the given n.
+@param head A pointer to a pointer to the head of the linked list.
+@param n The number of Fibonacci numbers to generate.
+@return 0 if successful, 1 if memory allocation failed.
+*/
+int create_fibonacci(Node **head, int n) {
+    unsigned long int fib[n];
+    int i;
+
+    /* If n is 0, insert "1" into the list and return. */
+    if (n == 0) {
+        insert_node(head, 1);
+        return 0;
+    }
+
+    /* If n is 1, insert "1" twice into the list and return. */
+    if (n == 1) {
+        insert_node(head, 1);
+        insert_node(head, 1);
+        return 0;
+    }
+
+    /* Insert "1" and "1" into the list to start the sequence. */
+    insert_node(head, 1);
+    insert_node(head, 1);
+    fib[0] = 1;
+    fib[1] = 1;
+
+    /*Calculate the n fib' numbers*/
+    for (i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+        printf("fib number %d is %lu\n", i, fib[i]);
+        /*Insert the new number into the linked list*/
+        insert_node(head, fib[i]);
+    }
+    /*If the head is null - error occurred while insert the new number into the list*/
+    if (head == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+/**
+
+Prints the contents of a linked list representing the Fibonacci sequence in reverse order.
+@param head A pointer to the head of the linked list.
+@param file_ptr A pointer to a file where the Fibonacci sequence will be printed.
+@param n The length of the Fibonacci sequence to print.
+*/
+void print_list(Node **head, FILE *file_ptr, int n) {
+    int i;
+    unsigned long int current_node_value;
+    Node *temp_node = *head;
+
+    fprintf(file_ptr, "\nHere is all the %d numbers the sequence:\n", n);
+    for (i = 0; i < n; i++) {
+        /*Print all the number from the biggest to the lowest*/
+        temp_node = temp_node->prev;
+        current_node_value = temp_node->fib_value;
+
+        /*Print to std*/
+        printf("%lu\n", current_node_value);
+
+        /*Print to file*/
+        fprintf(file_ptr, "%lu \n", current_node_value);
+    }
+}
+
+/**
+
+@brief Frees the memory allocated for a linked list of Node structs.
+This function frees the memory allocated for a linked list of Node structs,
+starting from the head node and traversing the list until the end node is reached.
+*/
+void free_list(Node *head) {
+    Node *current = head;
+    Node *next;
+
+    while (current != NULL && current->prev != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
 
