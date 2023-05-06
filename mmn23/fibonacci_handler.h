a47f355d4ead21127e7b338dabbaf64fc0d14b12
Almog Shtaigmann <a.shtaigmann@gmail.com>
2023-05-06 12:50:45 +0300

    First Version Of mmn23

--- mmn23/fibonacci_handler.h
+++ mmn23/fibonacci_handler.h
@@ -1,3 +1,14 @@
 //
 // Created by Almog Shtaigmann on 04/05/2023.
 //
+
+
+typedef struct node {
+    unsigned long fib_value;      /* A pointer to a string variable to store the Fibonacci number. */
+    struct node *next;  /* A pointer to the next node in the list. */
+    struct node *prev;  /* A pointer to the previous node in the list. */
+} Node;
+
+
+void create_fibonacci(Node **head, int n);
+
